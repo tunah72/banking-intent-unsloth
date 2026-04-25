06fd8e10ce664936b0861f9d14d5693b72df385e
tunah72 <duongtuananh0702@gmail.com>
2026-04-25 23:06:57 +0700

    feat: add llm_int8_skip_modules parameter to model quantization configuration

--- scripts/train.py
+++ scripts/train.py
@@ -67,6 +67,7 @@ def main(config_path):
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=compute_dtype,
         bnb_4bit_use_double_quant=True,
+        llm_int8_skip_modules=["score"],
     )
     
     model = AutoModelForSequenceClassification.from_pretrained(
